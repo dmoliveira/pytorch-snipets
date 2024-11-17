e0fdd9c7e943531408bec276de48731f6afe03e5
Diego Marinho <dmztheone@gmail.com>
2024-11-17 13:33:25 +1100

    Add proper evaluation module and refactor code

--- train_nn.py
+++ train_nn.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import utils
 import pandas as pd
 import torch
 import plotly.graph_objects as go
@@ -9,22 +10,33 @@ from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-DEVICE = torch.device("mps") if torch.backends.mps.is_built() else torce.device("cpu")
-
 # -- Utils ----------
-def difftime(start_time, unit: str="s"):
+def difftime(start_time, unit: str="s") -> int:
+    """Calculate difference in time in seconds or milliseconds."""
     if unit == "ms":
         return int((time()-start_time)/1000)
     else: # seconds
         return int(time()-start_time)
 
-def plot_loss(loss_history):
+def plot_loss(loss_history: list[float]):
+    """Display a line chart with the model loss over the iteration/epochs."""
     fig = go.Figure()
     fig.add_trace(go.Scatter(y=loss_history, mode='lines', name='Training Loss'))
     fig.update_layout(
         title="Training Loss Over Batches",
         xaxis_title="Batch Number",
         yaxis_title="Loss" )
+    fig.show()
+
+def plot_metrics(results: dict):
+    """Display main model metrics for test set."""
+    fig = go.Figure(data=[
+        go.Bar(name='Metrics', x=list(results.keys()), y=list(results.values())) ])
+    fig.update_layout(
+        title="Model Performance Metrics",
+        xaxis_title="Metric",
+        yaxis_title="Value",
+        yaxis_tickformat='.1f' )
     fig.show()
 
 # -- Data -----------
@@ -81,112 +93,168 @@ class EarlyStopping:
             if self.epochs_no_improve >= self.patience:
                 self.early_stop = True
 
-def train(dataloader, model, early_stopping, loss_fn, optimizer):
-    loss_history = []
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
+class Trainer():
 
-        X, y = X.to(DEVICE), y.to(DEVICE)
+    def __init__(self, model, early_stopping, loss_function, optimizer):
 
-        pred = model(X)
-        loss = loss_fn(pred, y)
+        self.device = utils.get_device()
+        self.model = model
+        model.to(self.device)
 
-        optimizer.zero_grad()
+        self.early_stopping = early_stopping
+        self.loss_function = loss_function
+        self.optimizer = optimizer
+        self.loss_history = []
+        self.epoch = 0
+
+    def reset(self):
+        self.loss_history = []
+        self.optimizer.zero_grad()
+
+    def train(self, dataloader, is_debug: bool=False):
+        self.epoch += 1
+        size = len(dataloader.dataset)
+        for batch, (X, y) in enumerate(dataloader):
+            _, loss = self.forward(X, y)
+            self.backward(loss)
+
+            loss, current = loss.item(), batch * len(X)
+            self.loss_history.append(loss)
+            if is_debug and current % 1000 == 0:
+                print(f"  • Loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+        #plot_loss(loss_history)
+
+    def forward(self, X, y):
+        _X, _y = X.to(self.device), y.to(self.device)
+        pred = self.model(_X)
+        loss = self.loss_function(pred, _y)
+        return pred, loss
+
+    def backward(self, loss):
+        self.optimizer.zero_grad()
         loss.backward()
-        optimizer.step()
+        self.optimizer.step()
 
-        loss, current = loss.item(), batch * len(X)
-        loss_history.append(loss)
-        if current % 1000 == 0:
-            print(f"  • Loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+from abc import ABC, abstractmethod
 
-    plot_loss(loss_history)
+class Evaluator():
+    def __init__(self):
+        pass
 
-def test(dataloader, model, loss_fn):
-    model.eval()
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss = 0
-    all_preds = []
-    all_labels = []
+    @abstractmethod
+    def predict(self):
+        """Calculate predictions, labels and losses."""
+        pass
 
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(DEVICE), y.to(DEVICE)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            all_preds.extend(pred.argmax(1).cpu().numpy())
-            all_labels.extend(y.cpu().numpy())
+    @abstractmethod
+    def calculate_metrics(self):
+        """Calculate all metrics related with your problem type (e.g., classification, prediction, etc)."""
+        pass
 
-    test_loss /= num_batches
+    def eval(self, dataloader, model):
+        """Execute the proper model evaluation."""
+        # predict and calculate metrics
+        all_preds, all_labels, avg_loss = self.predict(dataloader, model)
+        results = self.calculate_metrics(all_preds, all_labels, avg_loss)
+        # Display results in a table format
+        print(pd.DataFrame(results, index=["Metrics"]).T)
+        return results
+
+class EvaluatorClassification(Evaluator):
+
+    def __init__(self):
+        super().__init__()
+        self.accuracy = [] 
+        self.precision = [] 
+        self.recall = [] 
+        self.f1 = [] 
+        self.avg_loss = [] 
+
+    def predict(self, dataloader, trainer):
+        trainer.model.eval()
+        all_preds = []
+        all_labels = []
+        avg_loss = 0
+        with torch.no_grad():
+            for X, y in dataloader:
+                pred, loss = trainer.forward(X, y)
+                avg_loss += loss.item()
+                all_preds.extend(pred.argmax(1).cpu().numpy())
+                all_labels.extend(y.cpu().numpy())
+            avg_loss = avg_loss / len(dataloader)
+        return all_preds, all_labels, avg_loss
+
+    def calculate_metrics(self, all_labels, all_preds, avg_loss):
+        # Calculate metrics
+        accuracy = accuracy_score(all_labels, all_preds)
+        precision = precision_score(all_labels, all_preds, average='weighted', zero_division=0)
+        recall = recall_score(all_labels, all_preds, average='weighted', zero_division=0)
+        f1 = f1_score(all_labels, all_preds, average='weighted', zero_division=0)
+
+        self.accuracy.append(accuracy)
+        self.precision.append(precision)
+        self.recall.append(recall)
+        self.f1.append(f1)
+        self.avg_loss.append(avg_loss)
     
-    # Calculate metrics
-    accuracy = accuracy_score(all_labels, all_preds)
-    precision = precision_score(all_labels, all_preds, average='weighted', zero_division=0)
-    recall = recall_score(all_labels, all_preds, average='weighted', zero_division=0)
-    f1 = f1_score(all_labels, all_preds, average='weighted', zero_division=0)
-
-    # Prepare results in a dictionary
-    results = {
-        "Accuracy": accuracy * 100,  # percentage
-        "Precision": precision * 100,
-        "Recall": recall * 100,
-        "F1 Score": f1 * 100,
-        "Avg Loss": test_loss }
-
-    # Display results in a table format
-    results_df = pd.DataFrame(results, index=["Metrics"]).T
-    print(results_df)
-
-    # Plot results using Plotly
-    fig = go.Figure(data=[
-        go.Bar(name='Metrics', x=list(results.keys()), y=list(results.values())) ])
-    fig.update_layout(
-        title="Model Performance Metrics",
-        xaxis_title="Metric",
-        yaxis_title="Value",
-        yaxis_tickformat='.1f' )
-    fig.show()
-
+        # Prepare results in a dictionary
+        results = {
+            "Accuracy": accuracy * 100,  # percentage
+            "Precision": precision * 100,
+            "Recall": recall * 100,
+            "F1 Score": f1 * 100,
+            "Avg Loss": avg_loss }
+        return results
+    
 def main():
 
     start_time_total = time()
 
     # Parameters
     batch_size = 64
+    epochs = 1
     early_stopping = EarlyStopping(enabled=False, patience=3, delta=0.01)
-    enabled = early_stopping.enabled
-    patience = early_stopping.patience
-    delta = early_stopping.delta
     
     print("\nTRAIN-NN Pytorch Script")
     print("\nParameters")
     print(f"  • Batch-Size: {batch_size}")
-    print(f"  • Early-Stopping: enabled={enabled} patience={patience} delta={delta}")
+    print(f"  • Epochs: {epochs}")
+    print(f"  • Early-Stopping: ")
+    print(f"        • enabled={early_stopping.enabled}")
+    print(f"        • patience={early_stopping.patience}")
+    print(f"        • delta={early_stopping.delta}")
     print()
 
     # Load data
-    print("01/. Load Data")
+    print("01. Load Data")
     start_time_load_data = time()
     training_data, test_data = load_data(batch_size=batch_size)
     print(f"  Loaded in {difftime(start_time_load_data)} s")
 
     # Create the model
     model = NeuralNetwork()
-    model.to(DEVICE)
     loss_function = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+    trainer = Trainer(model, early_stopping, loss_function, optimizer)
+    evaluator = EvaluatorClassification()
+    results = None
 
-    print("\n02/. Model Training")
-    start_time_training = time()
-    train(training_data, model, early_stopping, loss_function, optimizer)
-    print(f"  Model trained in {difftime(start_time_training)} s")
-    
-    print("\n03/03. Model Testing")
-    start_time_test = time()
-    test(test_data, model, loss_function)
-    print(f"  Model testing in {difftime(start_time_test)} s")
+    print("\n02. Model Training")
+    total_time_training = 0 
+    for epoch in range(epochs):
+        print(f"\nEpoch {epoch+1}")
+        start_time_training = time()
+        trainer.train(training_data)
+        results = evaluator.eval(test_data, trainer)
+        time_epoch = difftime(start_time_training)
+        print(f"Epoch time {time_epoch} s")
+        total_time_training += time_epoch 
 
+    # Display training loss and test metrics charts 
+    #plot_loss(trainer.loss_history)
+    #plot_metrics(results)
+
+    print(f"\nModel trained in {total_time_training} s")
     print(f"\n--  Finished training in {difftime(start_time_total)} s")
 
 
